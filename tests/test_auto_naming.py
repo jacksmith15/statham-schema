c965838fb4cf37d73fb3c90013e660488a517077
Jack Smith <jack.smith@elucidata.co.uk>
2020-03-01 18:32:37 +0000

    Add test for composition of converters in arrays

--- tests/test_auto_naming.py
+++ tests/test_auto_naming.py
@@ -1,6 +1,8 @@
+from typing import List, Tuple, Type
+
 import pytest
 
-from tests.models.autoname import Autoname
+from tests.models.autoname import Autoname, ListAnyOfItem0, ListAnyOfItem1
 
 
 @pytest.mark.github_issue(29)
@@ -12,7 +14,24 @@ def test_anonymous_arrays_do_not_overwrite_themselves():
     )
 
 
-def test_anonymours_any_of_object_items_are_named_correctly():
-    assert Autoname(
-        list_any_of=[{"string_prop": "foo"}, {"integer_prop": "bar"}]
-    )
+class TestAnonymousAnyOfObjects:
+    @staticmethod
+    @pytest.fixture(scope="class")
+    def instance() -> Autoname:
+        return Autoname(
+            list_any_of=[{"string_prop": "foo"}, {"integer_prop": "bar"}]
+        )
+
+    @staticmethod
+    def test_successful_top_level_instantiation(instance: Autoname):
+        assert isinstance(instance, Autoname)
+
+    PARAMS: List[Tuple[int, Type]] = [(0, ListAnyOfItem0), (1, ListAnyOfItem1)]
+
+    @staticmethod
+    @pytest.mark.parametrize("index,model", PARAMS)
+    def test_successful_attribute_instantiation(
+        instance: Autoname, index: int, model: Type
+    ):
+        assert isinstance(instance.list_any_of, list)
+        assert isinstance(instance.list_any_of[index], model)
