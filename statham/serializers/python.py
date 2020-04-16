e7956ba24c11b086f862cb8a312837846629667b
Jack Smith <jack.smith@elucidata.co.uk>
2020-04-16 21:16:02 +0100

    Implement import detection

--- statham/serializers/python.py
+++ statham/serializers/python.py
@@ -1,5 +1,8 @@
-from statham.dsl.elements import Element
-from statham.orderer import orderer
+from typing import Set, Type, Union
+
+from statham.dsl.elements import Element, Object
+from statham.dsl.elements.meta import ObjectMeta
+from statham.orderer import orderer, get_children
 
 
 # TODO: Identify which of these to exclude when performing generation.
@@ -33,6 +36,76 @@ def serialize_python(*elements: Element) -> str:
     Captures declaration of the first Object elements, and any subsequent
     elements this depends on.
     """
-    return _IMPORT_STATEMENTS + "\n\n".join(
+    declarations = "\n\n".join(
         [object_model.python() for object_model in orderer(*elements)]
     )
+    imports = _get_imports(declarations, *elements)
+    return "\n\n\n".join(block for block in [imports, declarations] if block)
+
+
+def _get_imports(declarations: str, *elements: Element) -> str:
+    """Get import statements required by the DSL elements."""
+    imports = [
+        _get_standard_imports(declarations),
+        _get_statham_imports(declarations, *elements),
+    ]
+    return "\n\n".join(block for block in imports if block)
+
+
+def _get_standard_imports(declaration: str) -> str:
+    """Get group if imports from standard library.
+
+    Currently only includes type annotations.
+    """
+    type_imports = ", ".join(
+        [
+            annotation
+            for annotation in ("Any", "List", "Union")
+            if annotation in declaration
+        ]
+    )
+    if not type_imports:
+        return ""
+    return f"from typing import {type_imports}"
+
+
+def _get_statham_imports(declaration: str, *elements: Element) -> str:
+    """Construct imports from statham submodules."""
+    statham_imports = []
+    if "Maybe" in declaration:
+        statham_imports.append("from statham.dsl.constants import Maybe")
+    element_imports = _get_element_imports(*elements)
+    if element_imports:
+        statham_imports.append(element_imports)
+    if "Property" in declaration:
+        statham_imports.append("from statham.dsl.property import Property")
+    return "\n".join(statham_imports)
+
+
+def _get_element_imports(*elements: Element) -> str:
+    """Get the import string for DSL elements in use."""
+    prefix = "from statham.dsl.elements import "
+    max_length = 80
+    import_names = [
+        elem_type.__name__
+        for elem_type in set.union(
+            *(_get_single_element_imports(element) for element in elements)
+        )
+    ]
+    if not import_names:
+        return ""
+    imports = ", ".join(sorted(import_names))
+    if max_length < len(prefix) + len(imports):
+        imports = "\n    ".join(["("] + imports.split(" ")) + ",\n)"
+    return prefix + imports
+
+
+def _get_single_element_imports(
+    element: Element
+) -> Set[Union[Type[Element], ObjectMeta]]:
+    """Extract the set of element types used by a given element."""
+    get_type = (
+        lambda elem: Object if isinstance(elem, ObjectMeta) else type(elem)
+    )
+    children = [element, *get_children(element)]
+    return set(map(get_type, children))  # type: ignore
